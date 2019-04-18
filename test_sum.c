6e781ef85d27702443df42fb43f5bf39270215a8
Pushpam-iitd <35834848+Pushpam-iitd@users.noreply.github.com>
2019-04-18 19:23:25 +0530

    Add files via upload

---
+++ test_sum.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main ( int argc , char * argv [])
+{
+// If you follow the naming convention , the system call
+// name will be sys_toggle and you
+// call it by calling the function toggle ();
+int a =4;
+int b =3;
+add(a,b);
+// printf(1,"This is for test \n" );
+exit ();
+return 0;
+}
---
+++ types.h
@@ -0,0 +1,80 @@
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+typedef uint pde_t;
+
+
+#define MSGSIZE 8
+
+// code for queues
+
+
+#define NumberOfMessageQueues 64             // NPROCS is the number of message queues.
+#define NumberOfMessageBuffers 500
+
+
+typedef char* oneBuffer;       //the buffer of 8 bytes, have to malloc the space
+
+const int EndOfFreeList ;
+int lastBufferUsed;            // to keeptrack of the buffers used.
+
+struct intMessageQueue
+{
+	int front,last,size;      // struct of the meaage queue object
+	unsigned capacity;
+	int* arr; 				 // queue of int
+};
+
+
+
+struct waitQueueItem 
+{ 							// waitqueue item containes the pid of process
+    int pid; 				// and the buffer to recieve the message
+    char* buffer; 
+};
+
+
+struct waitQueue 
+{ 
+    int front, last, size;		// struct of the wait queue object
+    unsigned capacity; 
+    struct waitQueueItem* array;    // queue of waitqueue item
+};
+
+
+struct messageQueue 
+{ 
+    int front, last, size; 
+    unsigned capacity;
+    oneBuffer* array; 
+};
+
+
+oneBuffer messageBuffers[NumberOfMessageBuffers];    // define Number of Message Buffers
+int free_message_buffer;
+
+
+int message_queue_allocated[NumberOfMessageQueues];  // to define -> NumberOfMessageQueues
+struct waitQueue *wait_queue[NumberOfMessageQueues];
+struct intMessageQueue *int_message_queue[NumberOfMessageQueues];
+
+struct messageQueue *message_queue[NumberOfMessageQueues]; 
+
+// int isFull(struct messageQueue* mq);
+// int isEmpty(struct messageQueue* mq); 
+// void enqueue(struct messageQueue* mq, oneBuffer item); 
+// oneBuffer dequeue(struct messageQueue* mq); 
+int isFull(struct intMessageQueue* mq);
+int isEmpty(struct intMessageQueue* mq); 
+void enqueue(struct intMessageQueue* mq, int item); 
+int dequeue(struct intMessageQueue* mq); 
+
+int isWaitFull(struct waitQueue* mq); 
+int isWaitEmpty(struct waitQueue* mq); 
+void waitenqueue(struct waitQueue* mq, struct waitQueueItem item);
+struct waitQueueItem waitdequeue(struct waitQueue* mq); 
+void init_queues(void);
+void copyToSystemSpace(char* from,char* to, int len);
+void copyFromSystemSpace(char* to,char* from, int len);
+int getMessageBuffer(void);
+void freeMessageBuffer(int msg_no);
